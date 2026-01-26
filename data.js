497d324c93b048e602d67f1e982eacf75f8da3f2
Ethan-Davidi <ethan@hush.security>
2026-01-26 21:53:48 +0200

    Replace 'dog' and 'car' with 'bus' and 'fish'

--- data.js
+++ data.js
@@ -1,7 +1,8 @@
 const WORDS = [
   { word: "apple", img: "img/apple.png" },
   { word: "cat",   img: "img/cat.png" },
-  { word: "dog",   img: "img/dog.png" },
-  { word: "car",   img: "img/car.png" },
+  { word: "bus",   img: "img/bus.png" },
+  { word: "fish",   img: "img/fish.png" },
 ];
+
 
